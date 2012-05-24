9b687d77572a4e608705ca0562902bb872b91f6d
Dyson Simmons <dysonsimmons@gmail.com>
2012-05-24 17:31:36 +1000

    Added locals to link_to_add_fields so partial doesn't access instace variables.

--- lib/awesome-forms.rb
+++ lib/awesome-forms.rb
@@ -265,9 +265,10 @@ module AwesomeForms
 			hidden_field(:_destroy) + @template.link_to_function(name, "awesome_forms_remove_fields(this)", options)
 		end
 
-		def link_to_add_fields(name, association, association_object, partial, options = {})
+		def link_to_add_fields(name, association, association_object, partial, locals = {}, options = {})
 			fields = fields_for association, association_object, child_index: "new_#{association}" do |form|
-				@template.render partial: partial, object: form
+				locals.merge!({form: form})
+				@template.render partial: partial, locals: locals
 			end
 			link = @template.link_to_function name.html_safe, "awesome_forms_add_fields(this, \"#{association}\", \"#{@template.escape_javascript(fields)}\")", options
 			link = '<div class="awesome-forms-group">' + link + '</div>'
